e8f0123900cce666a2f5aded4810ca488d415207
luul negasi <luultesfay24@gmail.com>
2022-08-05 12:21:37 -0700

    change title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InputMovieForm from "./components/InputMovieForm";
 function App() {
   return (
     <div className="container">
-      <h1> MOVIES GALLERY</h1>
+      <h1> MOVIES GALLERY TO Search</h1>
       <InputMovieForm />
     </div>
   );
